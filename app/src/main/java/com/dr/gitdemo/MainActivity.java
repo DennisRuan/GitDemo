bf57cbb549d602c4a08a1a95db22a2fb8facc2b6
dennis <rwj011@163.com>
2019-05-11 23:16:45 +0800

    hellword

--- app/src/main/java/com/dr/gitdemo/MainActivity.java
+++ app/src/main/java/com/dr/gitdemo/MainActivity.java
@@ -1,7 +1,8 @@
 package com.dr.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvHello = findViewById(R.id.tv_hello);
     }
 }
